0ef2a7fcd390600d2b437b644fbb8fa92f5056e2
Captain-b <babakrezai@hotmail.com>
2022-02-05 21:53:54 +0000

    Remove unwanted import.

--- src/utils/twitter/config.ts
+++ src/utils/twitter/config.ts
@@ -1,7 +1,4 @@
-import Twitter from "twitter";
-import Twit from "twit";
-
-export const twitter: Twit.Options = {
+export const twitter = {
     consumer_key: process.env.TWITTER_API_KEY!,
     consumer_secret: process.env.TWITTER_API_SECRET!,
     bearer_token: process.env.TWITTER_BEARER_TOKEN!
