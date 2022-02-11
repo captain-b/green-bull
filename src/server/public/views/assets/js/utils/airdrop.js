253c22b00abb6d3f087021a8cc0a7ef903eeb49a
Babak Rezai <babakrezai@hotmail.com>
2022-02-11 23:39:41 +0000

    Fix claim issue.

--- src/server/public/views/assets/js/utils/airdrop.js
+++ src/server/public/views/assets/js/utils/airdrop.js
@@ -9,11 +9,27 @@ const airdropContract = () => {
 
 const claimAirdrop = async () => {
     try {
+        await ethereum.request({method: 'eth_requestAccounts'});
+        provider = new ethers.providers.Web3Provider(window.ethereum);
+        // alert(accs)
+        // // const provider = await detectEthereumProvider();
+        // alert(JSON.stringify(await provider.getSigner()))
         pendingTxAlert();
         const tx = await airdropContract().connect(await provider.getSigner()).claim();
         await tx.wait();
         txSubmittedAlert();
+
+        // pendingTxAlert();
+        // const tx = await airdropContract().connect(await provider.getSigner()).claim();
+        // await tx.wait();
+        // txSubmittedAlert();
     } catch (e) {
-        displayAlert(e.data.message, 'error');
+        // alert(JSON.stringify(e))
+        console.log(window.ethereum)
+        if (e.data && e.data.message) {
+            displayAlert(e.data.message, 'error');
+            return;
+        }
+        displayAlert('There was an unexpected error. Please use MetaMask on a desktop if the issue persists.', 'error');
     }
 }
